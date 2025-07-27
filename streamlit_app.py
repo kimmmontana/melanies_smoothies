a1423b6d14682faf74a825926c8da59952406436
Kim M. Montana <kimmontana23@gmail.com>
2025-07-27 09:41:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,11 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
+pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
+  
+
 ingredients_list =  st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe,
@@ -32,26 +37,22 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+  
+  
     my_insert_smt = """ insert into smoothies.public.orders(ingredients, name_on_order)
     values('""" + ingredients_string + """', '""" + name_on_order + """')"""
     
     #st.write(my_insert_smt)
     #st.stop()
-    pd_df=my_dataframe.to_pandas()
-    st.dataframe(pd_df)
-    st.stop()
-  
+    
     time_to_insert = st.button('submit order')
     
     if time_to_insert:
         session.sql(my_insert_smt).collect()
         st.success('Your Smoothie is ordered!', icon='✅')
-
-if ingredients_list:
-  ingredients_string = ''
-
-  for fruit_chosen in ingredients_list:
-    ingredients_string += fruit_chosen + ''
-    st.subheader(fruit_chosen + ' Nutrition Information')
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-    sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
